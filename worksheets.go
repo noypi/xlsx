34dc1dae54be8852dd6ef545b92159ba2f5dd760
Gogs <gogs@fake.local>
2017-02-07 09:47:16 +0800

    add PrintOut

--- worksheets.go
+++ worksheets.go
@@ -1,6 +1,8 @@
 package xlsx
 
 import (
+	"math"
+
 	"github.com/go-ole/go-ole"
 	"github.com/go-ole/go-ole/oleutil"
 )
@@ -18,5 +20,25 @@ func (this *Worksheet) Range(r string) (out *Range) {
 // toPage = set to 0 for default
 // copies = default is 1.
 func (this *Worksheet) PrintOut(fromPage, toPage, copies int, params ...interface{}) {
+	if fromPage < 1 {
+		fromPage = 1
+	}
+
+	var ps = make([]interface{}, 3)
+	ps[0] = fromPage
+	if toPage < fromPage {
+		ps[1] = math.MaxInt32
+	} else {
+		ps[1] = toPage
+	}
+	if copies <= 0 {
+		copies = 1
+	}
+	ps[2] = copies
+	if 0 == len(params) {
+		params = ps
+	} else {
+		params = append(ps, params...)
+	}
 	oleutil.MustGetProperty((*ole.IDispatch)(this), "PrintOut", params...)
 }
